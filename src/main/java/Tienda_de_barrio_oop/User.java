c3a9074f0a3ac56b58c8b53ceaabd06048e8b5a4
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2023-10-28 19:56:02 -0500

    ingresando excepciones y el modulo proveedor

--- src/main/java/Tienda_de_barrio_oop/User.java
+++ src/main/java/Tienda_de_barrio_oop/User.java
@@ -1,4 +1,165 @@
+
+
 package Tienda_de_barrio_oop;
 
+import Tienda_de_barrio_oop.Main;
+import Tienda_de_barrio_oop.Productos;
+import model.Proveedores;
+
+import java.util.*;
+
 public class User {
+    private String nameProv;
+    private int contactProv;
+
+    private int codeProv;
+    private String productProv;
+
+    public String getNameProv() {
+        return nameProv;
+    }
+
+    public void setNameProv(String nameProv) {
+        this.nameProv = nameProv;
+    }
+
+    public int getContactProv() {
+        return contactProv;
+    }
+
+    public void setContactProv(int contactProv) {
+        this.contactProv = contactProv;
+    }
+
+    public int getCodeProv() {
+        return codeProv;
+    }
+
+    public void setCodeProv(int codeProv) {
+        this.codeProv = codeProv;
+    }
+
+    public String getProductProv() {
+        return productProv;
+    }
+
+    public void setProductProv(String productProv) {
+        this.productProv = productProv;
+    }
+
+    private List<User> proveedores = new ArrayList<>();
+    Scanner impresion = new Scanner(System.in);
+
+    Main m = new Main();
+
+    public User(String nameProv, int contactProv, int codeProv, String productProv) {
+        this.nameProv = nameProv;
+        this.contactProv = contactProv;
+        this.codeProv = codeProv;
+        this.productProv = productProv;
+    }
+
+    public List<User> getProveedores() {
+        return proveedores;
+    }
+    public void MenuProductos (){
+        int seleccion = showOptionsMenuAndCaptureOption();
+        while(seleccion!=7)
+
+        {
+            switch (seleccion) {
+
+                case 1 -> IngresandoProveedor();
+                case 2 -> MostrarProveedores();
+              //  case 3 -> productos.ModificarProducto();
+               // case 4 -> productos.PrintProduct();
+             //   case 5 ->venta.Venta(productos);
+                //  case 6 -> Main.showOptionsMenuAndCaptureOptionPrincipal();
+
+
+            }
+            seleccion = showOptionsMenuAndCaptureOption();
+
+
+        }
+
+    }
+
+
+
+    private int showOptionsMenuAndCaptureOption() {
+        System.out.println("      Bienvenido " +
+                "\n" +
+                " /////////////\\\\\\\\\\\\\n" +
+                "(((((((((((((   \\\\\\\\\\\\\n" +
+                "))) ~~      ~~   (((\n" +
+                "((( (*)     (*)  )))\n" +
+                ")))     <        (((\\s\n" +
+                "((( '\\\\______/`   )))\\s\n" +
+                ")))\\\\___________/(((\\s\n" +
+                " (((   _)  (_    )))\\s\\s\n" +
+                "      /\\\\__/\\\\\"\"\" +");
+        System.out.println("±----------------------------------------±");
+        System.out.println("|   Administrador Mi Tienda de Barrio     |");
+        System.out.println("±----------------------------------------±");
+        System.out.println("1. Agregar producto                      |");
+        System.out.println("2. Eliminar producto                     |");
+        System.out.println("3. Actualizar o Modificar producto       |");
+        System.out.println("4. Ver todos los productos               |");
+        System.out.println("5. Validaciones Product                  |");
+        System.out.println("6. Salir                                 |");
+        System.out.println("±----------------------------------------±");
+        System.out.print("   Ingresa tu opción:    (1 - 5)  ");
+        Scanner scanner = new Scanner(System.in);
+        int seleccion = 6;
+        try {
+            seleccion = scanner.nextInt();
+            if (seleccion < 1 || seleccion > 6) {
+                System.out.println("| The Option selected is not valid. Please try again |");
+                showOptionsMenuAndCaptureOption();
+            }
+        } catch (Exception e) {
+            System.out.println("| The Option selected is not valid. Please try again |");
+            showOptionsMenuAndCaptureOption();
+        }
+        return seleccion;
+
+
+    }
+
+
+
+
+
+
+
+
+
+    public void IngresandoProveedor() {
+
+        System.out.print("Asigne un codigo al Proveedor ");
+        codeProv = impresion.nextInt();
+
+        System.out.print("Ingrese el nombre del Proveedor: ");
+        nameProv = impresion.next();
+        System.out.print("Ingrese el numero de contacto del proveedor: ");
+        contactProv = impresion.nextInt();
+        System.out.print("Ingrese el producto que vende el proveedor: ");
+        productProv = impresion.next();
+        System.out.println("Porveedor ingresado exitosamente");
+        proveedores.add(new User(nameProv,contactProv,codeProv,productProv));
+
+        // proveedores.stream().forEach(supplier -> System.out.println(supplier));
+        System.out.println(codeProv);
+    }
+
+
+    public void MostrarProveedores() {
+        System.out.println(" ingresan a mostrar");
+        proveedores.stream().forEach(supplier -> System.out.println(supplier));
+
+    }
 }
+
+
+
--- src/main/java/model/Venta.java
+++ src/main/java/model/Venta.java
@@ -2,30 +2,44 @@ package model;
 
 import Tienda_de_barrio_oop.AbstracProducto;
 import Tienda_de_barrio_oop.Productos;
-import java.util.Date;
-import java.security.PublicKey;
-import java.sql.SQLOutput;
-import java.util.List;
-import java.util.Scanner;
+import com.sun.security.jgss.GSSUtil;
 
-    public class Venta {
+import java.util.*;
+
+import java.security.PublicKey;
+
+
+public class Venta  {
 
         Scanner impresion = new Scanner(System.in);
-        Productos productos = new Productos();
-        public void Venta(){
-
-            productos.Inventario();
 
 
-            System.out.print("Por favor ingrese el codigo del producto a vender: ");
-            int codigoproducto = impresion.nextInt();
-            int pos =productos.getCodigo().indexOf(codigoproducto);
-            System.out.println("codigo    Producto     Unidades existentes     Precio ");
-            System.out.println(productos.getCodigo().get(pos) +"      "+ productos.getProduct().get(pos) +"       "+ productos.getCantidad().get(pos)+"              " + productos.getPrice().get(pos));
-            System.out.println("cuantas unidades va a compar");
-            int uniCompras = impresion.nextInt();
+        public void Venta( Productos productos){
+            if (productos.getProduct().isEmpty()) {
+                productos.PrintProduct();
+
+            }
+
+            System.out.println(" Ingrese el codigo del producto a vender");
+            int code = impresion.nextInt();
+            int altersearch= code;
+
+            Optional<Productos> MarketProduct = productos.getProduct().stream()
+                    .filter(market -> market.getCode() == altersearch)
+                    .findFirst();
+            productos.getProduct().stream().forEach(buy -> System.out.println(buy));
+
+
+            System.out.println("Por favor indique las unidades a comprar: ");
+            int units = impresion.nextInt();
             double iva = 0.19;
-            double suma = (uniCompras*productos.getPrice().get(pos));
+
+            double suma = productos.getProduct().stream()
+                    .filter(producto -> producto.getCode() == altersearch)
+                    .mapToDouble(producto -> units * producto.getPrice()) // Calcular la suma
+                    .sum();
+
+
             double totalIva =suma*iva;
             double total = suma+totalIva;
             Date date = new Date();
@@ -34,16 +48,15 @@ import java.util.Scanner;
             System.out.println("+-----------------------------+");
             System.out.println("|Venta                        ");
             System.out.println ("|"+date                     );
-            System.out.println ("|Producto:"+productos.getProduct().get(pos));
-            System.out.println("|Uniddes:"+uniCompras);
-            System.out.println("|Precio Unitario: "+productos.getPrice().get(pos)+"pesos");
+            System.out.print ("|Producto:");productos.getProduct().stream().map(Productos::getName).forEach(market -> System.out.println(market));
+            System.out.println("|Uniddes:"+units);
+            System.out.print("|Precio Unitario: ");productos.getProduct().stream().map(Productos::getPrice).forEach(marketP -> System.out.println(marketP));
             System.out.println("|Total sin IVA:"+suma+"  pesos");
             System.out.println("|IVA 19% :"+totalIva+" pesos");
             System.out.println("|Total:"+total);
             System.out.println("+-----------------------------------+");
             System.out.println("       Gracias por su compra ");
             System.out.println("+-----------------------------------+");
-
 
         }
 
